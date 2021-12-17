5200d9ccb8a2354c6a659738ce3fac70083d1349
DasVaca <sebastian23082000@gmail.com>
2021-12-17 09:25:59 -0300

    Triangle abc, struggle with tangent points

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const C_HEIGHT = 1000, C_WIDTH = 1000;
-const DFAULT_RADII = 70;
+const C_HEIGHT = 900, C_WIDTH = 1200;
+const DFAULT_RADII = 0.15*C_WIDTH;
 const DFAULT_COLOR = '#000';
 
 function Point(y, x) { return {y, x}; };
 function rInt(min, max) { return Math.floor(Math.random() * (max-min)); };
 function Circle(name, center, rad, color) { return {name, center, rad, color};};
 
-const c = document.querySelector("canvas");
-let ctx = c.getContext("2d");
-ctx.canvas.height = C_HEIGHT;
-ctx.canvas.width = C_WIDTH;
+const cCircles = document.querySelector("#canvas-circles-abc");
+const ctxCircles = cCircles.getContext("2d");
+
+const cTangentPoints = document.querySelector("#canvas-tangent-points-abc");
+const ctxTangentPoints = cTangentPoints.getContext("2d");
+
+const cTriangle = document.querySelector("#canvas-triangle-abc");
+const ctxTriangle = cTriangle.getContext("2d");
+
+ctxCircles.canvas.height = C_HEIGHT;
+ctxTangentPoints.canvas.height = C_HEIGHT;
+ctxTriangle.canvas.height = C_HEIGHT;
+
+ctxCircles.canvas.width = C_WIDTH;
+ctxTangentPoints.canvas.width = C_WIDTH;
+ctxTriangle.canvas.width = C_WIDTH;
+
+ctxCircles.font = "30px Arial";
+
+let A, B, C;
+let angleA;
 
 function refreshCirclesProperties() {
     let radii = [], color = [], names = ['A', 'B', 'C'];
@@ -19,10 +36,9 @@ function refreshCirclesProperties() {
 }
 
 function drawCircles() {
-    ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+    ctxCircles.clearRect(0, 0, C_WIDTH, C_HEIGHT);
 
     let properties = refreshCirclesProperties();
-    let A, B, C;
     [A, B, C] = (() => {
         let circles = [];
         properties.forEach(c => circles.push(Circle(c[0], Point(0,0), c[1], c[2])));
@@ -34,21 +50,60 @@ function drawCircles() {
     let AC = A.rad + C.rad;
     let BC = B.rad + C.rad;
 
-    let theta = Math.acos((AB**2 + AC**2 - BC**2)/(2*AB*AC)) / 2;
+    angleA = Math.acos((AB**2 + AC**2 - BC**2)/(2*AB*AC)) / 2;
 
-    A.center = Point(2*A.rad, 500);
-    B.center = Point(A.center.y + AB*Math.cos(theta), A.center.x - AB*Math.sin(theta));
-    C.center = Point(A.center.y + AC*Math.cos(theta), A.center.x + AC*Math.sin(theta));
-    
+    A.center = Point(1.5*A.rad, 0.5*C_WIDTH);
+    B.center = Point(A.center.y + AB*Math.cos(angleA), A.center.x - AB*Math.sin(angleA));
+    C.center = Point(A.center.y + AC*Math.cos(angleA), A.center.x + AC*Math.sin(angleA));
+
     [A, B, C].forEach(c => {
-        console.log(c);
-        ctx.strokeStyle = c.color;
-        ctx.beginPath();
-        ctx.arc(c.center.x, c.center.y, c.rad, 0, 6.3);
-        ctx.fillText(c.name, c.center.x-1, c.center.y+1);
-        ctx.stroke();
+        ctxCircles.strokeStyle = c.color;
+        ctxCircles.beginPath();
+        ctxCircles.arc(c.center.x, c.center.y, c.rad, 0, 6.3);
+        ctxCircles.fillText(c.name, c.center.x-1, c.center.y+1);
+        ctxCircles.stroke();
     });
 }
+
+document.querySelector("#show-tangent-abc").addEventListener("change", (e) => {
+    if (!e.target.checked) {
+        ctxTangentPoints.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+        return;
+    }
+    // Cosine law
+    let AB = A.rad + B.rad;
+    let AC = A.rad + C.rad;
+    let BC = B.rad + C.rad;
+    let angleB = Math.acos((AB**2 + BC**2 - AC**2)/(2*AB*BC)) / 2;
+    let angleComplement = Math.PI - angleB;
+
+    // Calculate Tangent Points
+    let tAB = Point(A.rad * Math.cos(angleA) + A.center.y, A.center.x - A.rad * Math.sin(angleA));
+    let tAC = Point(A.rad * Math.cos(angleA) + A.center.y, A.center.x + A.rad * Math.sin(angleA));
+    let tBC = Point(B.rad * Math.cos(angleComplement) + B.center.y, B.center.x + B.rad * Math.sin(angleComplement));
+
+    console.log({angleComplement, tBC});
+    // Draw
+    ctxTangentPoints.beginPath();
+    ctxTangentPoints.fillRect(tAB.x, tAB.y, 5, 5);
+    ctxTangentPoints.fillRect(tAC.x, tAC.y, 5, 5);
+    ctxTangentPoints.fillRect(tBC.x, tBC.y, 5, 5);
+    ctxTangentPoints.stroke();
+});
+
+document.querySelector("#show-triangle-abc").addEventListener("change", e => {
+    if (!e.target.checked) {
+        ctxTriangle.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+        return;
+    }
+
+    ctxTriangle.beginPath();
+    ctxTriangle.moveTo(A.center.x, A.center.y);
+    ctxTriangle.lineTo(C.center.x, C.center.y);
+    ctxTriangle.lineTo(B.center.x, B.center.y);
+    ctxTriangle.lineTo(A.center.x, A.center.y);
+    ctxTriangle.stroke();
+});
 
 document.querySelector("button").addEventListener("click", () => drawCircles());
 
